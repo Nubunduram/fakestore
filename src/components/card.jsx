c55eb424e25464dd2289d4e52f58513c052d307e
Nubunduram <ceantoine@hotmail.fr>
2025-03-31 11:55:25 +0200

    feat: PATCH product (Activité 6)

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -26,6 +26,24 @@ function ProductCard({ imgSrc, title, description, price }) {
         alert("« Le produit avec l'id " + data.id + " a été modifié ».")
     }
 
+    async function patchProduct() {
+        const product = {
+            id: 1,
+            price: 5
+        };
+        const response = await fetch(`https://fakestoreapi.com/products/${product.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(product)
+        });
+
+        const data = await response.json();
+
+        alert("« Le prix du produit n°" + data.id + " est désormais " + data.price + "€")
+    }
+
     return (
         <Card className="h-100">
             <Card.Img className="card-img" src={imgSrc} />
@@ -34,6 +52,7 @@ function ProductCard({ imgSrc, title, description, price }) {
                 <Card.Text>{description}</Card.Text>
                 <Card.Text>{price}€</Card.Text>
                 <Button variant="primary" onClick={() => putProduct()}>Modifier le produit complet</Button>
+                <Button variant="primary" onClick={() => patchProduct()}>Modifier le prix du produit</Button>
 
             </Card.Body>
 
