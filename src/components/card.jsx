e6aef799bbe501d8502212e54da8b3eb42018057
Nubunduram <ceantoine@hotmail.fr>
2025-03-31 12:02:31 +0200

    feat: DELETE product (Activité 7)

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -27,11 +27,13 @@ function ProductCard({ imgSrc, title, description, price }) {
     }
 
     async function patchProduct() {
+        const productId = 1;
+
         const product = {
-            id: 1,
             price: 5
         };
-        const response = await fetch(`https://fakestoreapi.com/products/${product.id}`, {
+
+        const response = await fetch(`https://fakestoreapi.com/products/${productId}`, {
             method: "PATCH",
             headers: {
                 "Content-type": "application/json"
@@ -44,6 +46,18 @@ function ProductCard({ imgSrc, title, description, price }) {
         alert("« Le prix du produit n°" + data.id + " est désormais " + data.price + "€")
     }
 
+    async function deleteProduct() {
+        const productId = 1;
+
+        const response = await fetch(`https://fakestoreapi.com/products/${productId}`, {
+            method: 'DELETE'
+        });
+
+        const data = await response.json();
+
+        alert(`« Le produit avec l'id ${data.id} a été supprimé ».`)
+    }
+
     return (
         <Card className="h-100">
             <Card.Img className="card-img" src={imgSrc} />
@@ -52,7 +66,8 @@ function ProductCard({ imgSrc, title, description, price }) {
                 <Card.Text>{description}</Card.Text>
                 <Card.Text>{price}€</Card.Text>
                 <Button variant="primary" onClick={() => putProduct()}>Modifier le produit complet</Button>
-                <Button variant="primary" onClick={() => patchProduct()}>Modifier le prix du produit</Button>
+                <Button variant="secondary" onClick={() => patchProduct()}>Modifier le prix du produit</Button>
+                <Button variant="danger" onClick={() => deleteProduct()}>Supprimer le produit</Button>
 
             </Card.Body>
 
