dba2ba0f1a25f9603faf4199faf5412fc4f9bae0
Nubunduram <ceantoine@hotmail.fr>
2025-03-31 11:38:19 +0200

    feat: Activité 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import ProductCard from "./components/card";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col } from "react-bootstrap";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -16,9 +16,33 @@ function App() {
     fetchProducts();
   }, []);
 
+  async function postProduct() {
+    const product = {
+      id: 1,
+      title: "T-shirt Basique",
+      price: 15,
+      description: "Un t-shirt basique",
+      category: "t-shirt",
+      image: "http://example.com"
+    };
+    const response = await fetch("https://fakestoreapi.com/products", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(product)
+    });
+
+    const data = await response.json();
+
+    alert("« Le produit avec l'id " + data.id + "a été créé ».")
+  }
+
+
   return (
     <>
       <h1 className="text-center">Products</h1>
+      <Button variant="primary" onClick={() => postProduct()}>Ajouter un produit</Button>
       <Container>
         <Row className="g-3">
           {products.map((product) => (
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,11 +4,11 @@ import Card from 'react-bootstrap/Card';
 function ProductCard({ imgSrc, title, description, price }) {
   return (
     <Card className="h-100">
-      <Card.Img className="card-img" variant="top" src={imgSrc} />
+      <Card.Img className="card-img" src={imgSrc} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{description}</Card.Text>
-        <Card.Text>${price}</Card.Text>
+        <Card.Text>{price}€</Card.Text>
       </Card.Body>
     </Card>
   );
